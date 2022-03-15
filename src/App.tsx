68cd39eeb07bd6c04637907148e73736aca59b3c
Beario <63807035+Beario@users.noreply.github.com>
2022-03-15 09:15:06 -0400

    all required files

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import "./App.css";
+import { useMemo } from "react";
+
+import Minter from "./Minter";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+  getMathWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { ThemeProvider, createTheme } from "@material-ui/core";
+
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const candyMachineId = process.env.REACT_APP_CANDY_MACHINE_ID
+  ? new anchor.web3.PublicKey(process.env.REACT_APP_CANDY_MACHINE_ID)
+  : undefined;
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const App = () => {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [getPhantomWallet(), getSolflareWallet(), getSolletWallet(), getMathWallet() ],
+    []
+  );
+
+  return (
+    <div>
+      <nav>
+        <div className="nav-container">
+        <a href="www.magpiemoguls.com"><img className="nav-logo" src="/img/logo.png" alt="" /></a>
+          <div className="social-icons hide-800">
+            <a href="www.magpiemoguls.com"><img className="nav-social" src="/icons/twitter.svg" alt="" /></a>
+            <a href="www.magpiemoguls.com"><img className="nav-social" src="/icons/discord.svg" alt="" /></a>
+          </div>
+        </div>
+      </nav>
+      <div className="content-wrapper">
+          <header id="link1">
+            <div>
+              <ThemeProvider theme={theme}>
+                <ConnectionProvider endpoint={endpoint}>
+                  <WalletProvider wallets={wallets} autoConnect>
+                    <WalletDialogProvider>
+                      
+                        <Minter
+                          candyMachineId={candyMachineId}
+                          
+                          connection={connection}
+                          startDate={startDateSeed}
+                          txTimeout={txTimeout}
+                          rpcHost={rpcHost}
+                        />
+                      
+                    </WalletDialogProvider>
+                  </WalletProvider>
+                </ConnectionProvider>
+              </ThemeProvider>
+            </div>
+          </header>
+      </div>
+    </div>
+  );
+};
+
+export default App;
